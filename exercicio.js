1460494ea8ef07076d23dbfb34ac1679a2b44da3
Gustavo Gonçalves da Rosa <142434921+gustavorosa13@users.noreply.github.com>
2023-08-28 17:10:05 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,67 @@
+/*let nome = prompt("Qual seu nome?");
+let cor = prompt("Qual sua cor favorita");*/
+
+/*console.log("A cor favorita de " + nome + "é" + cor);
+console.log(`A cor favorita de ${nome} ${cor}`); */
+
+/*console.log(nome.length);
+console.log(nome.toLowerCase());
+// Minúsculo
+console.log(nome.toUpperCase());
+// Maiusculo
+console.log(nome.trim());
+
+//método para buscar determinado texto dentro de uma string;
+console.log(nome.includes("cenoura"));
+
+console.log(nome.replaceAll("cenoura", "batata")); */
+
+/*let texto = prompt("Oque você faz nas horas vagas");
+
+console.log(texto.toUpperCase());
+console.log(texto.length);
+console.log(texto.replaceAll("o","i")); */
+
+//arrays
+/*let listaDeExercicios = ["elevacao lateral", "supino", "voador"];
+console.log(listaDeExercicios[0]);
+console.log(listaDeExercicios[1]);
+console.log(listaDeExercicios[2]);
+console.log(listaDeExercicios);
+
+let cachorros = ["Bulldogue", "Pastor-Alemão", "Labrador", "Husky", "Rottweiler"]
+
+let numero = prompt("Escolha um número de 0 a 4");
+
+console.log(cachorros[numero]);
+
+const pokemon = ["bulbasauro", "squirtle", "charmander"];
+console.log(pokemon.length);
+console.log(pokemon[0].length); */
+
+//-----------------------------------------------------
+
+/*const seriesBoas = ["Breaking Bad", "Brooklyn Nine-nine"];
+
+console.log(seriesBoas.includes("Breaking Bad"));
+console.log(seriesBoas.includes("Game of Thrones"));
+
+let instrumentos = ["violao", "guitarra", "bateria"];
+console.log(instrumentos);
+
+instrumentos.push("teclado");
+
+console.log(instrumentos);
+//instrumentos.pop();
+console.log(instrumentos);
+instrumentos.splice(2, 2);
+console.log(instrumentos); */
+
+
+let array = [1, 2, 3, 4, 5, 6];
+console.log(array.length);
+array.push(7);
+console.log(array);
+array.splice(3, 2);
+console.log(array);
+console.log(array.length);
